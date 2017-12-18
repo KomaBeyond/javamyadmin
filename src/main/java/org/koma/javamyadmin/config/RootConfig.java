6b87ed85107d53ff6173afe6ae95caea1cf5c02b
koma <komazhang@foxmail.com>
2017-12-18 12:25:33 +0800

    初始化项目

---
+++ src/main/java/org/koma/javamyadmin/config/RootConfig.java
@@ -0,0 +1,10 @@
+package org.koma.javamyadmin.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author koma <komazhang@foxmail.com>
+ */
+@Configuration
+public class RootConfig {
+}
