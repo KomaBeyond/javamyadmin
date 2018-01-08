2bac9526b977bbeeca68504a931c4c3b360bf400
koma <komazhang@foxmail.com>
2018-01-08 15:59:57 +0800

    shiro和spring集成完成

--- src/main/java/org/koma/javamyadmin/controller/IndexController.java
+++ src/main/java/org/koma/javamyadmin/controller/IndexController.java
@@ -1,17 +1,47 @@
 package org.koma.javamyadmin.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.koma.javamyadmin.Model.UserLogin;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author koma <komazhang@foxmail.com>
  */
 @Controller
-@RequestMapping({"/", "/javamyadmin"})
+@RequestMapping("/")
 public class IndexController {
-    @RequestMapping(method = RequestMethod.GET)
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(HttpServletRequest request, Model model) {
+        String exceptionClassName = (String)request.getAttribute("shiroLoginFailure");
+        String error = "";
+        if(UnknownAccountException.class.getName().equals(exceptionClassName)) {
+            error = "用户名/密码错误";
+        } else if(IncorrectCredentialsException.class.getName().equals(exceptionClassName)) {
+            error = "用户名/密码错误";
+        } else if(exceptionClassName != null) {
+            error = "其他错误：" + exceptionClassName;
+        }
+        model.addAttribute("error", error);
+        return "login";
+    }
+
+    @RequestMapping(value = "/do-login", method = RequestMethod.POST)
+    public String doLogin(@RequestBody UserLogin user, HttpServletRequest request) {
+        return "login";
+    }
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String main() {
+        return "main";
     }
 }
